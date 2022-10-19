cccc4829b5005ba532fc2d5d5d6b5652580a8255
mamama <2200556@ecccomp.com>
2022-10-19 12:28:29 +0900

    LoginActivity.kt passwordActivity.kt profileActivity.kt termsServiceActivity.kt timelineActivity.kt を追加

--- app/src/main/java/com/example/withme/timelineActivity.kt
+++ app/src/main/java/com/example/withme/timelineActivity.kt
@@ -3,10 +3,9 @@ package com.example.withme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class timelineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //10月19日
+        setContentView(R.layout.activity_timeline)
     }
 }
