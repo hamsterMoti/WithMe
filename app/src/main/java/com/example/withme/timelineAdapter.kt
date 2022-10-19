efb5dd566fa0e00b748282040f57abc833e34433
123rin <m.flower.2339@gmail.com>
2022-10-19 12:20:10 +0900

    add

---
+++ app/src/main/java/com/example/withme/timelineAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.withme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class timelineAdapter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline_adapter)
+    }
+}
