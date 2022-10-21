ec93b03198d7cacd838772ee23da9dd9ba8b9cbd
mamama <2200556@ecccomp.com>
2022-10-21 11:15:48 +0900

    manifest編集、ファイル追加、オバーフローメニュー追加

--- app/src/main/java/com/example/withme/timelineActivity.kt
+++ app/src/main/java/com/example/withme/timelineActivity.kt
@@ -1,7 +1,12 @@
 package com.example.withme
 
+import android.app.SearchManager
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -25,4 +30,29 @@ class timelineActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val timeline = Intent(this, timelineActivity::class.java)
+        val chat = Intent(this, chatActivity::class.java)
+        val notification = Intent(this, notificationActivity::class.java)
+        val mypage = Intent(this, mypageActivity::class.java)
+        val setting = Intent(this, settingActivity::class.java)
+        val logout = Intent(this, logoutActivity::class.java)
+
+        //連携処理を実施
+        when (item.itemId) {
+            R.id.timeline -> startActivity(timeline)
+            R.id.chat -> startActivity(chat)
+            R.id.notification -> startActivity(notification)
+            R.id.mypage -> startActivity(mypage)
+            R.id.setting -> startActivity(setting)
+            else -> startActivity(logout)//ログアウト処理を書く
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        getMenuInflater().inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
