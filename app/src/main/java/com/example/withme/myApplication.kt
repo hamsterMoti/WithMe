80bc9ee7b33d2238535f9061c69bb0e8733d324d
mamama <2200556@ecccomp.com>
2022-11-08 13:31:14 +0900

    loginActivity.ktのapi接続可能に変更 myApplication.ktのURLの定数と変数を追加 activity_login.xmlのパスワード変更を追加

--- app/src/main/java/com/example/withme/myApplication.kt
+++ app/src/main/java/com/example/withme/myApplication.kt
@@ -3,13 +3,31 @@ package com.example.withme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class myApplication : AppCompatActivity() {
-    var loginUserId: String = ""
-    var apiUrl: String = "http://click.ecc.ac.jp/ecc/whisper_e/"
-    val emptyError: String = "入力値がありません"
-    val notMatch : String = "ログインIDもしくはパスワードが異なります"
-    val connectionError : String = "インターネットに接続できませんでした"
-    val unexpectedError: String = "予期せぬエラーが発生しました"
+class myApplication{
+    val ipAddress = "34.229.155.198"
+    var apiUrl: String = "http://$ipAddress/with_me/"
+//  ID（メールアドレス）
+    var loginMyId: String = ""
+//  ニックネーム
+    var nickname : String = ""
+//  性別
+    var gender:Int = 0
+//  年齢
+    var age : Int = 0
+//  年(キャスト前）
+    var oldyear : String = ""
+//  月(キャスト前）
+    var oldmonth : String = ""
+//  日(キャスト前）
+    var oldday : String = ""
+//  年(キャスト後）
+    var year : String = ""
+//  月(キャスト後）
+    var month : String = ""
+//  日(キャスト後）
+    var day : String = ""
+
+
 
     companion object {
         private var instance : myApplication? = null
