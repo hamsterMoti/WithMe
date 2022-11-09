018866a3594db6683edf009aac0729e3fc7724e2
123rin <m.flower.2339@gmail.com>
2022-11-09 09:40:30 +0900

    オーバーフローメニュー

--- app/src/main/java/com/example/withme/notificationsettingActivity.kt
+++ app/src/main/java/com/example/withme/notificationsettingActivity.kt
@@ -1,7 +1,10 @@
 package com.example.withme
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.CompoundButton
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
@@ -36,5 +39,29 @@ class notificationsettingActivity : AppCompatActivity() {
         })
 
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val timeline = Intent(this, timelineActivity::class.java)
+        val chat = Intent(this, chatlistActivity::class.java)
+        val notification = Intent(this, notificationActivity::class.java)
+        val mypage = Intent(this, mypageActivity::class.java)
+        val settingsettinglist = Intent(this, settinglistActivity::class.java)
+        val login = Intent(this, loginActivity::class.java)
+
+        //連携処理を実施
+        when (item.itemId) {
+            R.id.timeline -> startActivity(timeline)
+            R.id.chat -> startActivity(chat)
+            R.id.notification -> startActivity(notification)
+            R.id.mypage -> startActivity(mypage)
+            R.id.setting -> startActivity(settingsettinglist)
+            else -> startActivity(login)//ログアウト処理を書く
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        getMenuInflater().inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/main/java/com/example/withme/postActivity.kt
+++ app/src/main/java/com/example/withme/postActivity.kt
@@ -4,6 +4,8 @@ import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -150,6 +152,30 @@ class postActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val timeline = Intent(this, timelineActivity::class.java)
+        val chat = Intent(this, chatlistActivity::class.java)
+        val notification = Intent(this, notificationActivity::class.java)
+        val mypage = Intent(this, mypageActivity::class.java)
+        val settingsettinglist = Intent(this, settinglistActivity::class.java)
+        val login = Intent(this, loginActivity::class.java)
+
+        //連携処理を実施
+        when (item.itemId) {
+            R.id.timeline -> startActivity(timeline)
+            R.id.chat -> startActivity(chat)
+            R.id.notification -> startActivity(notification)
+            R.id.mypage -> startActivity(mypage)
+            R.id.setting -> startActivity(settingsettinglist)
+            else -> startActivity(login)//ログアウト処理を書く
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        getMenuInflater().inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
 }
