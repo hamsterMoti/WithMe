da3a4f53841a4eff1598cb91e04640209adebb8a
mamama <2200556@ecccomp.com>
2022-12-01 13:00:08 +0900

    新規作成可能

--- app/src/main/java/com/example/withme/createaccountActivity.kt
+++ app/src/main/java/com/example/withme/createaccountActivity.kt
@@ -3,33 +3,30 @@ package com.example.withme
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class createaccountActivity : AppCompatActivity() {
     val client = OkHttpClient()
     val myApp = myApplication.getInstance()
     val errormsg = errorApplication.getInstance()
-
     private lateinit var birthday: TextView
-    var yearLog = ""
-    var monthLog = ""
-    var dayLog = ""
-
-    var cal: Calendar = Calendar.getInstance()
-    // 年を取得
-    val yyyy = cal.get(Calendar.YEAR)
-    // 月を取得(ただし0〜11のため、1を加算する)
-    val mm = cal.get(Calendar.MONTH)
-    // 日を取得
-    val dd = cal.get(Calendar.DAY_OF_MONTH)
-
+    val cal: Calendar = Calendar.getInstance()
+    var oldBirthday = 0
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,17 +39,21 @@ class createaccountActivity : AppCompatActivity() {
         val nextButton = findViewById<Button>(R.id.nextButton)
         val genderGroup = findViewById<RadioGroup>(R.id.genderRadioGroup)
         val menRadio = findViewById<RadioButton>(R.id.menRadioButton)
-        val id = genderGroup.checkedRadioButtonId
-        var number = 0
+        var genderText = ""
+        var num = 0
+        var a = ""
         val emptyError = errormsg.emptyError
 //      生年月日
         birthday = findViewById<TextView>(R.id.birthday)
 
-        birthday.text = "$yyyy/$mm/$dd"
+        var year = cal.get(Calendar.YEAR)
+        var month = cal.get(Calendar.MONTH) + 1
+        var day = cal.get(Calendar.DATE)
+        birthday.text = "$year/$month/$day"
+
 
         birthday.setOnClickListener{
             showDatePicker()
-
         }
 
         genderGroup.check(menRadio.id)
@@ -62,52 +63,63 @@ class createaccountActivity : AppCompatActivity() {
                 nicknameEdit.error = emptyError
             } else if (mailaddressEdit.text.toString().isEmpty()) {
                 mailaddressEdit.error = emptyError
-            }else if(yearLog.isEmpty()){
-                birthday.error = emptyError
 
             } else {
                 val nickname = nicknameEdit.text.toString()
                 val myId = mailaddressEdit.text.toString()
+                val id = genderGroup.checkedRadioButtonId
+
                 when(id) {
                     R.id.menRadioButton -> {//男
-                        number = 1
-                    }
-                    R.id.womanRadioButton -> {//女
-                        number = 2
+                        genderText = "男"
+                    }R.id.womanRadioButton -> {//女
+                        genderText = "女"
+
                     }
                 }
-                Log.v("num",number.toString())
-                myApp.loginMyId = myId
-//                myApp.nickname = nickname
-//                myApp.year = yearLog.toInt().toString()
-                val num1 = 0
-                Log.v("j", yearLog.toInt().toString())
-//                myApp.month = monthLog.toInt().toString()
-//                myApp.day = dayLog.toInt().toString()
+//                myApp.loginMyId = myId
+                var birthGetText = birthday.text
+                //生年月日をString型に変換
+                birthGetText = birthGetText.toString()
+                //生年月日をフォーマットする
+                val formatter = DateTimeFormatter.ofPattern("yyyy-[]M-[]d")
+                birthGetText = LocalDate.parse(birthGetText, formatter).toString()
+
+
                 val intent = Intent(this, passwordActivity::class.java)
+                intent.putExtra("userId",myId)
+                intent.putExtra("userName",nickname)
+                intent.putExtra("birthday",birthGetText)
+                intent.putExtra("gender",genderText)
+                Log.v("genderText",genderText)
                 startActivity(intent)
+                Log.v("tag", birthGetText)
+
             }
 
         }
     }
-    @SuppressLint("SetTextI18n")
+
     private fun showDatePicker() {
+        var ye = ""
+        var mon = ""
+        var day = ""
+        val yyyy = cal.get(Calendar.YEAR)
+        val mm = cal.get(Calendar.MONTH)
+        val dd = cal.get(Calendar.DAY_OF_MONTH)
         val datePickerDialog = DatePickerDialog(
             this,
             DatePickerDialog.OnDateSetListener() { view, year, month, dayOfMonth->
-                birthday.text = "${year}/${month + 1}/${dayOfMonth}"
-                yearLog = "$year"
-                monthLog = "$month"
-                dayLog = "$dayOfMonth"
+                birthday.text = "${year}-${month + 1}-${dayOfMonth}"
+                ye = "${year}"
+                mon ="${month+1}"
+                day = "${dayOfMonth}"
             },
             yyyy,
             mm,
             dd)
-        datePickerDialog.show(
-
-        )
+        datePickerDialog.show()
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val timeline = Intent(this, timelineActivity::class.java)
         val chat = Intent(this, chatlistActivity::class.java)
--- app/src/main/java/com/example/withme/passwordActivity.kt
+++ app/src/main/java/com/example/withme/passwordActivity.kt
@@ -1,17 +1,24 @@
 package com.example.withme
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import okhttp3.OkHttpClient
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class passwordActivity : AppCompatActivity() {
     val client = OkHttpClient()
     val myApp = myApplication.getInstance()
     val errormsg = errorApplication.getInstance()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password)
@@ -20,18 +27,33 @@ class passwordActivity : AppCompatActivity() {
         val repasswordEdit = findViewById<EditText>(R.id.repasswordEdit)
         val nextButton = findViewById<Button>(R.id.nextButton)
         val emptyError = errormsg.emptyError
+        val myId = intent.getStringExtra("userId")
+        val nickname = intent.getStringExtra("userName")
+        var birthday = intent.getStringExtra("birthday")
+        val gender = intent.getStringExtra("gender")
 
+//        val formatter = DateTimeFormatter.ofPattern("yyyy/[]M/[]d")
+//        birthday = LocalDate.parse(birthday, formatter).toString()
+//        Log.v("date",birthday.toString())
         nextButton.setOnClickListener {
             if (passwordEdit.text.toString().isEmpty()) {
                 passwordEdit.error = emptyError
             } else if (repasswordEdit.text.toString().isEmpty()) {
                 repasswordEdit.error = emptyError
+            }else if(passwordEdit.text.toString() != repasswordEdit.text.toString()){
+                passwordEdit.error = errormsg.notMatch
+                repasswordEdit.error = errormsg.notMatch
             } else {
-                val intent = Intent(this, termsServiceActivity::class.java)
-//                myApp.loginUserId = passwordEdit
-                startActivity(intent)
+                if(myId != null) {
+                    val pass = passwordEdit.text.toString()
+                    val intent = Intent(this, termsServiceActivity::class.java)
+                    val UserURL =
+                        myApp.apiUrl + "userAdd.php?userId=$myId&userName=$nickname&password=$birthday&birthday=$birthday&gender=$gender"
+                    intent.putExtra("UserURL", UserURL)
+                    myApp.loginMyId = myId
+                    startActivity(intent)
+                }
             }
         }
-
     }
 }
