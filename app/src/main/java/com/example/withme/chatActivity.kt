dba054b3d36f592c5ea97f8c5ce99035357d2483
mamama <2200556@ecccomp.com>
2022-12-05 15:05:09 +0900

    チャット画面追加

--- app/src/main/java/com/example/withme/chatActivity.kt
+++ app/src/main/java/com/example/withme/chatActivity.kt
@@ -3,13 +3,156 @@ package com.example.withme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+import java.lang.reflect.Array.getInt
 
 class chatActivity : AppCompatActivity() {
+    val client = OkHttpClient()
+    val myApp = myApplication.getInstance()
+    val errormsg = errorApplication.getInstance()
+
+    private lateinit var messageRecyclerView:RecyclerView
+    private lateinit var messageBox:EditText
+    private lateinit var sendButton:ImageView
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList:ArrayList<Message>
+
+    var receiverRoom:String? = null
+    var sendRoom:String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+        supportActionBar //バーを隠す
+            ?.hide()
+//        intentでroomNoを受け取る
+//        val roomNo = intent.getStringExtra("UserURL")
+        val roomNo = 22
+//        val userId = getInt("userId")
+        val userId = "2200166@ecc.ac.jp"
+        messageRecyclerView = findViewById(R.id.chatRecycle)
+        messageBox = findViewById(R.id.chatEditText)
+        sendButton = findViewById(R.id.sendImage)
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this,messageList)
+
+//        チャット更新
+        val URL = myApp.apiUrl+"chat.php?roomNo=$roomNo"
+        // http接続開始
+        val request = Request.Builder().url(URL).build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                this@chatActivity.runOnUiThread {
+                    Toast.makeText(applicationContext, errormsg.connectionError, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val csvStr = response.body!!.string()
+                val resultError = JSONObject(csvStr)
+                if (resultError.getString("result") == "error") {
+                    this@chatActivity.runOnUiThread {
+                        Toast.makeText(
+                            applicationContext,
+                            errormsg.notMatch,
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                } else if (resultError.getString("result") == "success") {
+                    this@chatActivity.runOnUiThread {
+                        val date =resultError.getJSONArray("chatList")
+                        //データが存在する間listにデータを挿入する
+                        for (i in 0 until date.length()) {
+                            val json = date.getJSONObject(i)
+                            val myId = json.getString("userId")
+                            val userName = json.getString("userName")
+                            var icon = json.getString("icon")
+                            val message = json.getString("message")
+                            var image = json.getString("image")
+                            var messageDate = json.getString("messageDate")
+
+                            messageList.add(Message(message,userId))
+                        }
+
+                        messageRecyclerView.layoutManager = LinearLayoutManager(applicationContext)
+                        val adapter = MessageAdapter(this@chatActivity ,messageList)
+                        messageRecyclerView.adapter = adapter
+
+                    }
+                }
+            }
+        })
+//        仮データ
+        val senderUid = "000"
+//        送信ボタン
+        sendButton.setOnClickListener{
+            val message = messageBox.text.toString()
+//            URLのパラメータ
+//            userId+roomNo+message
+            val messageURL = myApp.apiUrl+"chatPost.php?userId=$userId&roomNo=$roomNo&message=$message"
+            Log.v("message",messageURL)
+            // http接続開始
+            val request = Request.Builder().url(messageURL).build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    this@chatActivity.runOnUiThread {
+                        Toast.makeText(applicationContext, errormsg.connectionError, Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val csvStr = response.body!!.string()
+                    val resultError = JSONObject(csvStr)
+                    if (resultError.getString("result") == "error") {
+                        this@chatActivity.runOnUiThread {
+                            Toast.makeText(
+                                applicationContext,
+                                errormsg.notMatch,
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }
+                    } else if (resultError.getString("result") == "success") {
+                        this@chatActivity.runOnUiThread {
+                            val date =resultError.getJSONArray("chatList")
+                            //データが存在する間listにデータを挿入する
+                            for (i in 0 until date.length()) {
+                                val json = date.getJSONObject(i)
+                                val myId = json.getString("userId")
+                                val userName = json.getString("userName")
+                                var icon = json.getString("icon")
+                                val message = json.getString("message")
+                                var image = json.getString("image")
+                                var messageDate = json.getString("messageDate")
+
+                                messageList.add(Message(message,userId))
+                            }
+
+                            messageRecyclerView.layoutManager = LinearLayoutManager(applicationContext)
+                            val adapter = MessageAdapter(this@chatActivity ,messageList)
+                            messageRecyclerView.adapter = adapter
+
+                        }
+                    }
+                }
+            })
+
+
+        }
+//        戻るボタン
+
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
---
+++ app/src/main/java/com/example/withme/chatMessage/MessageAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.withme
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageAdapter(val context: Context,val messageList:ArrayList<Message>):
+    RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    val myApp = myApplication.getInstance()
+    val ITEM_RECEIVE = 1
+    val ITEM_SENT = 2
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if(viewType == 1){
+//            receive
+            val view: View = LayoutInflater.from(context).inflate(R.layout.receive_msg,parent,false)
+            return  ReceiveMegHolder(view)
+        }else{
+//            sent
+            val view: View = LayoutInflater.from(context).inflate(R.layout.send_msg,parent,false)
+            return  SentViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val currentMessage = messageList[position]
+
+        if(holder.javaClass == SentViewHolder :: class.java){
+            val viewHolder = holder as SentViewHolder
+            holder.sentMessage.text = currentMessage.message
+        }else{
+            val viewHolder = holder as ReceiveMegHolder
+            holder.receiveMessage.text = currentMessage.message
+
+        }
+    }
+
+    override fun getItemViewType(position: Int):Int{
+        val currentMessage = messageList[position]
+        if(myApp.loginMyId == currentMessage.senderId){
+            return ITEM_SENT
+//        if("aaa@bbb.com" == currentMessage.senderId){
+//            return ITEM_SENT
+        }else{
+            return  ITEM_RECEIVE
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return messageList.size
+    }
+    class SentViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val sentMessage = itemView.findViewById<TextView>(R.id.sendText)
+    }
+    class ReceiveMegHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+        val receiveMessage = itemView.findViewById<TextView>(R.id.receiveText)
+
+    }
+
+}
