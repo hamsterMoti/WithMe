271499e6b6aa3d14beb197d346c82c2f086a6114
mamama <2200556@ecccomp.com>
2022-12-15 10:58:18 +0900

    ログイン画面修正

--- app/src/main/java/com/example/withme/loginActivity.kt
+++ app/src/main/java/com/example/withme/loginActivity.kt
@@ -34,49 +34,51 @@ class loginActivity : AppCompatActivity() {
 
 
         loginButton.setOnClickListener {
-//            if (userEdit.text.toString().isEmpty()) {
-//                userEdit.error = emptyError
-//            } else if (passEdit.text.toString().isEmpty()) {
-//                passEdit.error = emptyError
-//            } else {
-            val myId = userEdit.text.toString()
-            val pass = passEdit.text.toString()
+            if (userEdit.text.toString().isEmpty()) {
+                userEdit.error = emptyError
+            } else if (passEdit.text.toString().isEmpty()) {
+                passEdit.error = emptyError
+            } else {
+                val myId = userEdit.text.toString()
+                val pass = passEdit.text.toString()
 //                URLを変更
-            Log.v("ID", myId)
-            Log.v("pass", pass)
+                val URL = "${myApp.apiUrl}loginAuth.php?userId=$myId&password=$pass"
 
-            val URL = "${myApp.apiUrl}loginAuth.php?userId=$myId&password=$pass"
-
-            val request = Request.Builder().url(URL).build()
-            client.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    this@loginActivity.runOnUiThread {
-                        Toast.makeText(applicationContext, errormsg.connectionError, Toast.LENGTH_SHORT).show()
-                    }
-                }
-                override fun onResponse(call: Call, response: Response) {
-                    val csvStr = response.body!!.string()
-                    val resultError = JSONObject(csvStr)
-                    if (resultError.getString("result") == "error") {
+                val request = Request.Builder().url(URL).build()
+                client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
                         this@loginActivity.runOnUiThread {
-                            val error = resultError.getString("errMsg")
-                            Toast.makeText(applicationContext, error, Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                    } else if (resultError.getString("result") == "success") {
-                        this@loginActivity.runOnUiThread {
-                            val myApp = myApplication.getInstance()
-                            myApp.loginMyId = myId
-                            Log.v("myId",URL)
-                            val intent = Intent(applicationContext, timelineActivity::class.java)
-                            startActivity(intent)
+                            Toast.makeText(
+                                applicationContext,
+                                errormsg.connectionError,
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
                     }
-                }
-            })
-//            }空白チェック
+
+                    override fun onResponse(call: Call, response: Response) {
+                        val csvStr = response.body!!.string()
+                        val resultError = JSONObject(csvStr)
+                        if (resultError.getString("result") == "error") {
+                            this@loginActivity.runOnUiThread {
+                                val error = resultError.getString("errMsg")
+                                Toast.makeText(applicationContext, error, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        } else if (resultError.getString("result") == "success") {
+                            this@loginActivity.runOnUiThread {
+                                val myApp = myApplication.getInstance()
+                                myApp.loginMyId = myId
+                                Log.v("myId", URL)
+                                val intent =
+                                    Intent(applicationContext, timelineActivity::class.java)
+                                startActivity(intent)
+                            }
+                        }
+                    }
+                })
+            }
         }
-
         createAccountText.setOnClickListener {
             val intent = Intent(this, createaccountActivity::class.java)
             startActivity(intent)
@@ -86,28 +88,5 @@ class loginActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val timeline = Intent(this, timelineActivity::class.java)
-        val chat = Intent(this, chatActivity::class.java)
-        val notification = Intent(this, notificationActivity::class.java)
-        val mypage = Intent(this, mypageActivity::class.java)
-        val setting = Intent(this, settingActivity::class.java)
-        val login = Intent(this, loginActivity::class.java)
-
-        //連携処理を実施
-        when (item.itemId) {
-            R.id.timeline -> startActivity(timeline)
-            R.id.chat -> startActivity(chat)
-            R.id.notification -> startActivity(notification)
-            R.id.mypage -> startActivity(mypage)
-            R.id.setting -> startActivity(setting)
-            else -> startActivity(login)//ログアウト処理を書く
-        }
-        return super.onOptionsItemSelected(item)
-    }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        getMenuInflater().inflate(R.menu.menu, menu)
-        return super.onCreateOptionsMenu(menu)
     }
 }
